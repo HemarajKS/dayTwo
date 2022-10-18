d6d7b337b80265427f846f07bb9bf4b645cb016a
HemarajKS <hemaraja.shettigar@robosoftin.com>
2022-10-18 13:15:07 +0530

    afternoon

--- src/components/homeBody/homeBody.tsx
+++ src/components/homeBody/homeBody.tsx
@@ -1,7 +1,27 @@
 import './homeBody.css'
 
 const HomeBody = () => {
-  return <div className="homeBody">HomeBody</div>
+  return (
+    <div className="homeBodyContainer">
+      <div className="homeBodyHeader">
+        <div className="headerBodyTitle">Sites</div>
+        <div className="headerBodyOptions">
+          <div className="headerSearch">
+            <input type="text" className="searchbar" placeholder="Search" />
+            <img
+              src={require('../../assets/icons/search.png')}
+              alt="search"
+              className="searchbarIcn"
+            />
+          </div>
+          <div className="headerAddButton">
+            <img src={require('../../assets/icons/add_btn.png')} alt="add" />
+          </div>
+        </div>
+      </div>
+      <div className="homeBodyContents">Body</div>
+    </div>
+  )
 }
 
 export default HomeBody
